ca33a27914b435082392c5332db78e82f7795fda
unknown <mishradivyanshu7@gmail.com>
2022-07-21 15:45:39 +0530

    basic loops

---
+++ Python/loops.py
@@ -0,0 +1,24 @@
+# a = int(input("Enter the value of a:"))
+# print(a)
+# Code to find the sum of squares of each element of the list using for loop:
+
+# numbers = [3, 5, 23, 6, 5, 1, 2, 9, 8]  
+  
+# initializing a variable that will store the sum  
+# sum_ = 0  
+  
+# using for loop to iterate over the list  
+# for num in numbers:  
+     
+#     sum_ = sum_ + num ** 2   
+  
+# print("The sum of squares is: ", sum_)
+
+numbers = [3, 5,4]
+
+
+
+for num in numbers:
+    sum_init=0
+    sum_init=sum_init + num **2
+print("The sum of squares is: ", sum_init)
---
+++ Python/rough.py
@@ -0,0 +1,7 @@
+numbers = [3, 5]
+
+sum_init=0
+
+for num in numbers:
+    sum_init=sum_init + num **2
+print("The sum of squares is: ", sum_init)
